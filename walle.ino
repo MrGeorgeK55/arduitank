65477a12ce7afb837fd8736bad220474d60be03f
MrGeorgeK55 <georgek55_55@hotmail.com>
2024-05-22 22:14:31 -0600

    updated description of the code and the todo list

--- walle.ino
+++ walle.ino
@@ -1,3 +1,20 @@
+// Im to lazy to write a proper README.md file, so here is a brief description of the code.
+// This code is for a robot that can be controlled using an Android app via Bluetooth.
+// The robot has two DC motors for tank drive and an elevator motor for lifting objects.
+// The robot can move forward, backward, left, right, stop, move the elevator up, down, and stop the elevator.
+// The robot is controlled using the HC-05 Bluetooth module and an Android app.
+// The Android app sends commands to the robot using Bluetooth serial communication.
+// The robot is powered by an Arduino Uno board and an L298N motor driver for the DC motors on the wheels and a BTS7960 H-Bridge for the elevator motor.
+
+
+// todo 
+// get a proper power source for the motors
+// it should be 6v  for the wheels and 12v for the elevator
+// i need to test the amps required for the motors at full speed and load
+// still thinking about  if it should move continously and tell them to stop each time or in move while the button is pressed and stop when released
+// also i need to test the speed of the motors and the elevator and get a good but stable speed for the robot
+// the motor seem to have also a rotatory encoder so i can get the speed of the motors and the elevator i will adapt this later...
+
 // Motor A (Tank Drive Left)
 const int motorA1 = 7;
 const int motorA2 = 6;
